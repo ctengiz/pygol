e6b710a4761204d4b53d218f4e8f139eac3fced3
Çağatay Tengiz <cagatay.tengiz@makki.com.tr>
2015-04-30 02:29:21 +0300

    Initial attempt
    I still feel sth is wrong with the logic...

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Game of life in python
 
 Canvas update code borrowed from : http://codereview.stackexchange.com/questions/26885/animation-with-5x5-map-of-cubes
+
+
+
+ 1
+101
+ 1
--- pygol.py
+++ pygol.py
@@ -13,6 +13,9 @@ class Gol:
 
         self.alive_cell = "#00FF00"
         self.dead_cell = "#808080"
+        self.blank_cell = "#FFFFFF"
+
+        self.grid = [[0 for x in range(cols)] for y in range(rows)]
 
         self.init_grid()
 
@@ -28,7 +31,60 @@ class Gol:
     def init_grid(self):
         for cl in range(self.cols):
             for rw in range(self.rows):
-                color = self.random_color()
+
+                is_alive = random.randint(0, 1)
+                if is_alive == 1:
+                    self.grid[cl][rw] = is_alive
+                else:
+                    self.grid[cl][rw] = -1 #empty cell
+
+                if is_alive:
+                    color = self.alive_cell
+
+                    x1 = cl * self.size
+                    y1 = rw * self.size
+                    x2 = x1 + self.size
+                    y2 = y1 + self.size
+
+                    self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=color)
+
+    def tick(self):
+        for cl in range(self.cols):
+            for rw in range(self.rows):
+                n_alive = 0
+
+                for x in range(-1, 2):
+                    for y in range(-1, 2):
+                        nx = cl + x
+                        ny = rw + y
+                        if (nx >= 0) \
+                                and (ny >= 0) \
+                                and (nx < self.rows) \
+                                and (ny < self.cols) \
+                                and ((nx != cl) or (ny != rw)):
+                            if self.grid[nx][ny] == 1:
+                                n_alive += 1
+
+                if self.grid[cl][rw] == 1:
+                    #if cell is alive !
+                    if n_alive < 2:
+                        self.grid[cl][rw] = 0
+                    if n_alive > 3:
+                        self.grid[cl][rw] = 0
+
+                if self.grid[cl][rw] == 0:
+                #else:
+                    if n_alive == 3:
+                        self.grid[cl][rw] = 1
+
+                if self.grid[cl][rw] == 1:
+                    color = self.alive_cell
+
+                if self.grid[cl][rw] == 0:
+                    color = self.dead_cell
+
+                if self.grid[cl][rw] == -1:
+                    color = self.blank_cell
 
                 x1 = cl * self.size
                 y1 = rw * self.size
@@ -37,19 +93,9 @@ class Gol:
 
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=color)
 
-    def tick(self):
-
-        x1 = 10
-        y1 = 10
-        x2 = x1 + self.size
-        y2 = y1 + self.size
-
-        color = self.random_color()
-        print(color)
-
-        self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=color)
-
         self.root.after(25, self.tick)
+
+
 
 Gol(50, 50, 10)
 
